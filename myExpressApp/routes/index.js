50afedf3c85834eddf1a0fab58ab9b5cb5e60695
Zilong Qiu <ziqixa@syntronic.com>
2023-08-23 14:30:18 -0400

    yyy

--- myExpressApp/routes/index.js
+++ myExpressApp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', abcV: process.env.ABC });
+  res.render('index', { title: 'Express', abcV: process.env.ABC, ACTIVATE_URL:process.env.ACTIVATE_URL, RESET_URL:process.env.RESET_URL, SENSATIVE_OFFSET_FLAG:process.env.SENSATIVE_OFFSET_FLAG, NEWDF:process.env.NEWDF  });
 });
 
 module.exports = router;
